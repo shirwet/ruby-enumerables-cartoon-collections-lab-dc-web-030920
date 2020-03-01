16b54040fed3a6470f28d5151c773de9776aab25
shirwet Elfeel <eshirwet@yahoo.com>
2020-03-01 00:07:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.select { |num| calls > 4}
+  calls.select { |num| calls.length > 4}
 end
 
 def find_the_cheese# code an argument here
